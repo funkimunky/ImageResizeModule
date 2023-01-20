4c845699fc0226b21e6b96066e2e54a96a985458
David Thomson <davidthomson123@hotmail.com>
2023-01-20 10:22:45 +0000

    Appveyor Commit test

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -1,4 +1,4 @@
-# This script will invoke pester tests
+# This script will invoke pester tests 
 # It should invoke on PowerShell v2 and later
 # We serialize XML results and pull them in appveyor.yml
 
