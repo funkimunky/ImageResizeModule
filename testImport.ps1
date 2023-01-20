5ba76f18854cde3040b301723f9828a545d3423f
David Thomson <davidthomson123@hotmail.com>
2023-01-20 16:51:16 +0000

    edit testimport

--- testImport.ps1
+++ testImport.ps1
@@ -6,7 +6,12 @@ $longerSide = 2500
 Write-Log "Started processing $(Get-Date -Format u)" #change this to output log file
 $ExcelPaths = Get-pathfile -IncludeExcludePath $PSScriptRoot
 $paths = Get-Imagepaths -ExcelPaths $ExcelPaths
-$image_list = Get-imagelist -paths $paths -Width $longerSide -Height $longerSide -batch 1000
-Resize-Image -ImagePath $image_list -Longerside $longerSide -OverWrite
-Write-Log "end processing $(Get-Date -Format u)"  #change this to output log file
-Write-Log "Origional storage used $Global:OrigionalTotal MB : Storage used after compression $Global:FinalTotal MB)"  #change this to output log file
+$image_list = Get-imagelist -paths $paths -Width $longerSide -Height $longerSide -batch 5000
+if($null -ne $image_list){
+    Resize-Image -ImagePath $image_list -Longerside $longerSide -OverWrite   
+    Write-Log "Origional storage used $Global:OrigionalTotal MB : Storage used after compression $Global:FinalTotal MB)"  #change this to output log file
+    Write-Log "end processing $(Get-Date -Format u)"  #change this to output log file
+}else{
+    Write-Log "No images to process"  #change this to output log file
+    Write-Log "end processing $(Get-Date -Format u)"  #change this to output log file    
+}
