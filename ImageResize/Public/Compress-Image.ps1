8adc36c5ebfe0ad0a60abc4e061f910212d895e0
David Thomson <davidthomson123@hotmail.com>
2023-01-20 16:06:13 +0000

    Remove commented out code

--- ImageResize/Public/Compress-Image.ps1
+++ ImageResize/Public/Compress-Image.ps1
@@ -8,24 +8,13 @@
     param(
         [Parameter(Mandatory = $True)][System.String]$type,
         [Parameter(Mandatory = $True)][string]$path,
-        # [Parameter(Mandatory = $False)][Switch]$report,
         [Parameter(Mandatory = $False, ParameterSetName = "Longerside")][Int]$Longerside
     )
 
     $params = switch ($type) {
-        "jpg" { 'mogrify -quality 82 -resize {0}x{0}' -f $Longerside }
-        # "gif" { "-fuzz 10% -layers Optimize" }
-        # "png" { "-depth 24 -define png:compression-filter=2 -define png:compression-level=9 -define png:compression-strategy=1" }
+        "jpg" { 'mogrify -quality 82 -resize {0}x{0}' -f $Longerside }       
     }
-
-    # if ($report) {
-    #     # Write-Output ""
-    #     # Write-Output "Listing $type files that would be included for compression with params: $params"
-    # } else {
-    #     # Write-Output ""
-    #     # Write-Output "Compressing $type files with parameters: $params"
-    # }
-    
+   
     Get-Item $path -Include "*.$type" | 
         Where-Object {
             $_.Length/1kb -gt $minSize
@@ -33,27 +22,7 @@
         Sort-Object -Descending length |
         ForEach-Object {
             $file = "'" + $_.FullName + "'"
-        
-            # if ($report) {
-            #     # $fSize = Get-Size-Kb($file)
-            #     # Write-Output "$file - $fSize"
-            # } else {
-            #     if ($verbose) {
-            #         # Write-Output "Compressing $file"
-            #         # $fileStartSize = Get-Size-Kb($file)
-            #     }
-        
-            #     # compress image
-            #     if ($report -eq $False) {
-                    Invoke-Expression "magick $params $file"
-                # }
-
-                # if ($verbose) {
-                #     # $fileEndSize = Get-Size-Kb($file)
-                #     # Write-Output "Reduced from $fileStartSize to $fileEndSize"
-                # }
-
-                $Global:FinalTotal += Get-Size-Item-mb($path)                
-            # }
+            Invoke-Expression "magick $params $file"
+            $Global:FinalTotal += Get-Size-Item-mb($path) 
         }
 }
--- ImageResize/Public/Resize-Image.ps1
+++ ImageResize/Public/Resize-Image.ps1
@@ -9,88 +9,51 @@
     (
         [Parameter(Mandatory = $True)][String[]]$ImagePath,
         [Parameter(Mandatory = $False)][Switch]$MaintainRatio,
-        [Parameter(Mandatory = $False, ParameterSetName = "Longerside")][Int]$Longerside,
-        # [Parameter(Mandatory = $False, ParameterSetName = "Absolute")][Int]$Height,
-        # [Parameter(Mandatory = $False, ParameterSetName = "Absolute")][Int]$Width,        
-        # [Parameter(Mandatory = $False, ParameterSetName = "Percent")][Double]$Percentage,       
+        [Parameter(Mandatory = $False, ParameterSetName = "Longerside")][Int]$Longerside, 
         [Parameter(Mandatory = $False)][String]$NameModifier = "resized",
         [Parameter(Mandatory = $False)][System.Management.Automation.SwitchParameter]$OverWrite
     )
     
     Begin 
     {
-        $Global:OrigionalTotal
-
-        # If ($Width -and $Height -and $MaintainRatio) {
-        #     Throw "Absolute Width and Height cannot be given with the MaintainRatio parameter."
-        # }
- 
-        # If (($Width -xor $Height) -and (-not $MaintainRatio)) {
-        #     Throw "MaintainRatio must be set with incomplete size parameters (Missing height or width without MaintainRatio)"
-        # }
- 
-        # If ($Percentage -and $MaintainRatio) {
-        #     Write-Warning "The MaintainRatio flag while using the Percentage parameter does nothing"
-        # }
-
-        # If ($Longerside -and $Width -or $Longerside -and $height) {
-        #     Throw "Should only be longer side in pixels"
-        # }
-
-        # If ($Percentage -and $Longerside -or $MaintainRatio -and $Longerside) {
-        #     Throw "Percentage or maintain ratio cannot be used with longerside pixels flag"
-        # }
-
-        
+        $Global:OrigionalTotal        
     }
     Process 
     {
-        
-      
-            ForEach ($Image in $ImagePath) {                
-                $Path = (Resolve-Path $Image).Path
-                $Dot = $Path.LastIndexOf(".")
-                
-                try 
-                {
-                    switch ($OverWrite.IsPresent) {
-                        $true {
-                                # Overite images
-                                $OutputPath = $Path.Substring(0, $Dot) + $Path.Substring($Dot, $Path.Length - $Dot)
-                            }
-                        $false {
-                                # rename images
-                                $OutputPath = $Path.Substring(0, $Dot) + "_" + $NameModifier + $Path.Substring($Dot, $Path.Length - $Dot)
-                            }
-                                        
-                    }
-
-                    $Global:OrigionalTotal += Get-Size-Item-mb($Image)
-
+        ForEach ($Image in $ImagePath) {                
+            $Path = (Resolve-Path $Image).Path
+            $Dot = $Path.LastIndexOf(".")
             
-                    If ($PSCmdlet.ShouldProcess("Resized image based on $Path", "save to $OutputPath")) {  
-                        # If ($MaintainRatio) {                
-                        
-                        # }
-
-                        # If ($Percentage) {
-                        
-                        # }
-
-                        If ($Longerside) {
-                            Compress-Image -type "jpg" -path $OutputPath -Longerside $Longerside
-                        }                   
-                            
-                    }   
-                
+            try 
+            {
+                switch ($OverWrite.IsPresent) {
+                    $true {
+                            # Overite images
+                            $OutputPath = $Path.Substring(0, $Dot) + $Path.Substring($Dot, $Path.Length - $Dot)
+                        }
+                    $false {
+                            # rename images
+                            $OutputPath = $Path.Substring(0, $Dot) + "_" + $NameModifier + $Path.Substring($Dot, $Path.Length - $Dot)
+                        }
+                                    
                 }
-                catch 
-                {
-                    Write-Log -Text $Image
-                    Write-Log -Text $_.Exception.Message
-                    Throw "$($_.Exception.Message)"
-                }
+
+                $Global:OrigionalTotal += Get-Size-Item-mb($Image)
+        
+                If ($PSCmdlet.ShouldProcess("Resized image based on $Path", "save to $OutputPath")) {
+                    If ($Longerside) {
+                        Compress-Image -type "jpg" -path $OutputPath -Longerside $Longerside
+                    }
+                }   
+            
             }
+            catch 
+            {
+                Write-Log -Text $Image
+                Write-Log -Text $_.Exception.Message
+                Throw "$($_.Exception.Message)"
+            }
+        }
       
         
     }
