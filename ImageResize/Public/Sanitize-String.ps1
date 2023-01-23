641f5d6a1a618ef9a59ea453b07c987ad294fff0
David Thomson <davidthomson123@hotmail.com>
2023-01-23 15:25:03 +0000

    add sanitize string function

---
+++ ImageResize/Public/Sanitize-String.ps1
@@ -0,0 +1,20 @@
+﻿Function Sanitize-String{
+    [CmdLetBinding(
+        SupportsShouldProcess = $true, 
+        PositionalBinding = $false,
+        ConfirmImpact = "Low",
+        DefaultParameterSetName = "Absolute"
+    )]
+    Param 
+    (
+        [Parameter(Mandatory = $True)][String[]]$path
+    )  
+
+    $charsToSanitize = @{"'" = "``'"}
+
+    foreach($key in $charsToSanitize){
+        $path[0] = $path[0].Replace($key, $charsToSanitize[$key])
+    }
+    
+    return $path
+}
